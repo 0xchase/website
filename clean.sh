cdf32b7ae6b030283b570a27765a5b74fbbe0f64
chase1635321 <chasekanipe@gmail.com>
2020-04-15 12:43:50 -0400

    done

---
+++ clean.sh
@@ -0,0 +1,1 @@
+rm cybersecurity.html physics.html gamedesign.html synthesizers.html vsts.html piano.html filmscoring.html blog.html index.html
--- generate.py
+++ generate.py
@@ -2,55 +2,69 @@
 
 import os
 
-os.system("rm cybersecurity.html")
+def main():
+    generateMalwareTable()
+    os.system("pwd")
+    os.system("cp skeleton/index.html index.html")
+    os.system("cp skeleton/physics.html physics.html")
+    os.system("cp skeleton/gamedesign.html gamedesign.html")
+    os.system("cp skeleton/synthesizers.html synthesizers.html")
+    os.system("cp skeleton/vsts.html vsts.html")
+    os.system("cp skeleton/piano.html piano.html")
+    os.system("cp skeleton/blog.html blog.html")
+    os.system("cp skeleton/filmscoring.html filmscoring.html")
 
-os.system("ls malware-reports/* > temp.txt")
+def generateMalwareTable():
+    os.system("rm cybersecurity.html")
+    os.system("ls malware-reports/* > temp.txt")
 
-files = ""
-with open("temp.txt", "r") as f:
-    files = f.read().split("\n")
-os.system("rm temp.txt")
+    files = ""
+    with open("temp.txt", "r") as f:
+        files = f.read().split("\n")
+    os.system("rm temp.txt")
 
-html1 = ""
-html2 = ""
+    html1 = ""
+    html2 = ""
 
-with open("skeletons/cybersecurity.html", "r") as f:
-    html1 = f.read()
+    with open("skeletons/cybersecurity.html", "r") as f:
+        html1 = f.read()
 
-tabledata = ""
-for f in files:
-    name = f.split("/")[-1]
-    tabledata += """
+    tabledata = ""
+    for f in files:
+        name = f.split("/")[-1]
+        tabledata += """
+                                <tr>
+                                    <td>""" + name + """</td>
+                                    <td><a href=" """ + f + """ " target="_blank">""" + name + """</a></td>
+                            </tr>
+    """
+
+
+    table = """
+    <h3>Malware Analysis</h3>
+    <div class="table-wrapper">
+            <table>
+                    <thead>
                             <tr>
-                                <td>""" + name + """</td>
-                                <td><a href=" """ + f + """ " target="_blank">""" + name + """</a></td>
-                        </tr>
-"""
+                                    <th>Sample</th>
+                                    <th>Writeup</th>
+                            </tr>
+                    </thead>
+                    <tbody>
+                    """ + tabledata + """
+                    </tbody>
+            </table>
+    </div>
+    """
+    for line in html1.split("\n"):
+        if "Malware Table" in line:
+            html2 += table
+        else:
+            html2 += line + "\n"
 
+    with open("temp.html", "w") as f:
+        f.write(html2)
 
-table = """
-<h3>Malware Analysis</h3>
-<div class="table-wrapper">
-        <table>
-                <thead>
-                        <tr>
-                                <th>Sample</th>
-                                <th>Writeup</th>
-                        </tr>
-                </thead>
-                <tbody>
-                """ + tabledata + """
-                </tbody>
-        </table>
-</div>
-"""
-for line in html1.split("\n"):
-    if "Malware Table" in line:
-        html2 += table
-    else:
-        html2 += line + "\n"
+    os.system("mv temp.html cybersecurity.html")
 
-with open("temp.html", "w") as f:
-    f.write(html2)
-
-os.system("mv temp.html cybersecurity.html")
+main()
