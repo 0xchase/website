9cee4c9819dc86c703dec7107a3a9f642209d85a
chase1635321 <chasekanipe@gmail.com>
2020-04-15 12:48:46 -0400

    done

--- generate.py
+++ generate.py
@@ -4,15 +4,6 @@ import os
 
 def main():
     generateMalwareTable()
-    os.system("pwd")
-    os.system("cp skeleton/index.html index.html")
-    os.system("cp skeleton/physics.html physics.html")
-    os.system("cp skeleton/gamedesign.html gamedesign.html")
-    os.system("cp skeleton/synthesizers.html synthesizers.html")
-    os.system("cp skeleton/vsts.html vsts.html")
-    os.system("cp skeleton/piano.html piano.html")
-    os.system("cp skeleton/blog.html blog.html")
-    os.system("cp skeleton/filmscoring.html filmscoring.html")
 
 def generateMalwareTable():
     os.system("rm cybersecurity.html")
